ef8e2f7df166551cc15c9124021c0a738406fb38
Nauman3S <naumanshakir3s@gmail.com>
2022-01-15 10:46:50 +0500

    fileDownload function with autodetect extensions

--- Firmware/main.py
+++ Firmware/main.py
@@ -5,12 +5,23 @@
  #no borders fullscreen no pointer
 
 from urllib.request import urlopen
+import requests
+import time
   
 # import json
 import json
-# store the URL in url as 
+# store the URL in url as
 # parameter for urlopen
 jsonVals=None
+def fileDownload(fileName, urlV):
+    print('downloading file')
+    urlValues=urlV.split('.')
+    fileExtension=urlValues[len(urlValues)-1]
+    url = urlV
+    r = requests.get(url, allow_redirects=True)
+    open(fileName+'.'+fileExtension, 'wb').write(r.content)
+    print('file downloaded')
+
 def callAPI():
     url = "https://mediascheduler-backend.production.rehanshakir.com/api/fields"
   
@@ -22,6 +33,13 @@ def callAPI():
     data_json = json.loads(response.read())
     return data_json
 
-jsonVals=callAPI()
-print(len(jsonVals))
-print(jsonVals[0])
+while True:
+    jsonVals=callAPI()
+    # print(len(jsonVals))
+    #print(jsonVals[0])
+    for i in range(0,len(jsonVals)):
+        if('default' in jsonVals[i]['fileName']):
+            fileDownload('media/default',jsonVals[i]['url'])
+            
+
+    time.sleep(10)
